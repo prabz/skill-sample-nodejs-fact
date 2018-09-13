7f9820fc90398d362bb2fb36ca6114d488a6f015
Prabhuram <prabz@users.noreply.github.com>
2018-09-13 16:11:37 +0530

    Update index.js

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -8,11 +8,11 @@ const cookbook = require('./alexa-cookbook.js');
 //TODO: The items below this comment need your attention.
 //=========================================================================================================================================
 
-const SKILL_NAME = 'Space Facts';
-const GET_FACT_MESSAGE = 'Here\'s your fact: ';
-const HELP_MESSAGE = 'You can say tell me a space fact, or, you can say exit... What can I help you with?';
+const SKILL_NAME = 'Safety Save';
+const GET_FACT_MESSAGE = 'What do you have to report ';
+const HELP_MESSAGE = 'You can say tell me a safety save, or, you can say exit... What can I help you with?';
 const HELP_REPROMPT = 'What can I help you with?';
-const FALLBACK_MESSAGE = 'The Space Facts skill can\'t help you with that.  It can help you discover facts about space if you say tell me a space fact. What can I help you with?';
+const FALLBACK_MESSAGE = 'The Safety save skill can\'t help you with that.  It can help you record safety concerns in your FC if you say record a safety save. What can I help you with?';
 const FALLBACK_REPROMPT = 'What can I help you with?';
 const STOP_MESSAGE = 'Goodbye!';
 
@@ -21,19 +21,7 @@ const STOP_MESSAGE = 'Goodbye!';
 //=========================================================================================================================================
 
 const data = [
-  'A year on Mercury is just 88 days long.',
-  'Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.',
-  'Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.',
-  'On Mars, the Sun appears about half the size as it does on Earth.',
-  'Earth is the only planet not named after a god.',
-  'Jupiter has the shortest day of all the planets.',
-  'The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.',
-  'The Sun contains 99.86% of the mass in the Solar System.',
-  'The Sun is an almost perfect sphere.',
-  'A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.',
-  'Saturn radiates two and a half times more energy into space than it receives from the sun.',
-  'The temperature inside the Sun can reach 15 million degrees Celsius.',
-  'The Moon is moving approximately 3.8 cm away from our planet every year.',
+  'Please note if this is an actionable item,area,equipment,etc that could cause an incident or is unsafe in it/\'s current state.If the oval is checked,the issue will be addressed in 48 hours.Please provide all the information to me"
 ];
 
 //=========================================================================================================================================
